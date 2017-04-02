d66a50fef44cfe38538d51cda914967b1f3b882a
moulax <fabien.palicot@gmail.com>
2017-04-02 23:11:03 +0200

    rename user en utilisateur pour problème postgres

--- grails-app/init/foodscore/BootStrap.groovy
+++ grails-app/init/foodscore/BootStrap.groovy
@@ -1,18 +1,22 @@
 package foodscore
 
+import grails.util.Environment
+
 class BootStrap {
 
     def init = { servletContext ->
-        Utilisateur.bootstrap()*.save(failOnError: true, flush: true)
-        Role.bootstrap()*.save(failOnError: true, flush: true)
-        UtilisateurRole.bootstrap()*.save(failOnError: true, flush: true)
-        TypeMenu.bootstrap()*.save(failOnError: true, flush: true)
-        Unite.bootstrap()*.save(failOnError: true, flush: true)
-        Famille.bootstrap()*.save(failOnError: true, flush: true)
+        if (Environment.current == Environment.DEVELOPMENT) {
+            Utilisateur.bootstrap()*.save(failOnError: true, flush: true)
+            Role.bootstrap()*.save(failOnError: true, flush: true)
+            UtilisateurRole.bootstrap()*.save(failOnError: true, flush: true)
+            TypeMenu.bootstrap()*.save(failOnError: true, flush: true)
+            Unite.bootstrap()*.save(failOnError: true, flush: true)
+            Famille.bootstrap()*.save(failOnError: true, flush: true)
 
-        Aliment.bootstrap()*.save(failOnError: true, flush: true)
-        Menu.bootstrap()*.save(failOnError: true, flush: true)
-        MenuAliment.bootstrap()*.save(failOnError: true, flush: true)
+            Aliment.bootstrap()*.save(failOnError: true, flush: true)
+            Menu.bootstrap()*.save(failOnError: true, flush: true)
+            MenuAliment.bootstrap()*.save(failOnError: true, flush: true)
+        }
     }
 
     def destroy = {
